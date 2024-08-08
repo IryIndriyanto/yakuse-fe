4d7df82b43bfcf5e4e00b332decda50e00175d0b
Gustitreshana <gustith2105@gmail.com>
2024-08-09 03:17:02 +0700

    add navbar and footer (temporary)

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="font-serif bg-[#FCFCFC] p-10 flex justify-between border-t-[1px] border-[#FD5F00]">
+      <div className="flex flex-col justify-center gap-4">
+        <div>
+          <h1 className="text-5xl font-bold text-[#40ABFF]">YAKUSE</h1>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-16">
+        <p>2024 © Yakuse</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
