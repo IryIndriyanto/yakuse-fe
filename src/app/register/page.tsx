dd85f4c19461a5c0c3e756732391066db2c2f91d
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-08-09 06:10:03 -0700

    fix: media query for login register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,13 +5,13 @@ const RegisterPage = () => {
   return (
     <main className="h-[100vh] flex items-center justify-center">
       <div className="flex items-center justify-center gap-16">
-        <RegisterForm className="w-full basis-1/2"/>
+        <RegisterForm className="w-full basis-1/2" />
         <Image
           src={"/assets/images/two-business-man.png"}
           width={700}
           height={700}
           alt="two businessman talking"
-          className="hidden lg:block lg:basis-1/2"
+          className="mq825:hidden block basis-1/2"
         />
       </div>
     </main>
