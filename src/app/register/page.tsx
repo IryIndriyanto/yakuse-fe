ce565f4b8a762849d2aa95ad90a990ffaa013101
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-08-10 22:20:41 -0700

    fix: handle responsiveness

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,16 +3,18 @@ import RegisterForm from "./RegisterForm";
 
 const RegisterPage = () => {
   return (
-    <main className="h-[100vh] flex items-center justify-center">
-      <div className="flex items-center justify-center gap-16">
+    <main className="flex items-center justify-center px-32 py-12">
+      <div className="w-full h-full flex items-center justify-center gap-16">
         <RegisterForm className="w-full basis-1/2" />
-        <Image
-          src={"/assets/images/two-business-man.png"}
-          width={700}
-          height={700}
-          alt="two businessman talking"
-          className="mq825:hidden block basis-1/2"
-        />
+
+        <div className="mq825:hidden w-full h-full object-contain block basis-1/2">
+          <Image
+            src={"/assets/images/two-business-man.png"}
+            width={700}
+            height={700}
+            alt="two businessman talking"
+          />
+        </div>
       </div>
     </main>
   );
