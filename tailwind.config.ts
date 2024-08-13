8f00a1979f41ea6a54b961b9787fadf786bedcdc
arisarae <playarisa24@gmail.com>
2024-08-13 18:04:04 +0700

    updating tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,68 +1,79 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
-        'primary-p-three': '#fcfcfc',
-        'black-b-two': '#525455',
-        'primary-p-one': '#40abff',
-        'primary-p-four': '#fd5f00',
-        'black-b-one': '#1b1c1d',
-        'white-w-two': '#e6f5ff',
-        black: '#000',
-        'primary-p-two': '#005792',
-        steelblue: {
-          '100': '#267db8',
-          '200': 'rgba(38, 125, 184, 0.09)',
+        "black": "#000",
+        // changing "black-b-..." to "b-..."
+        "b-one": "#1b1c1d",
+        "b-two": "#525455",
+        // changing "primary-p-..." to "p-..."
+        "p-one": "#40abff",
+        "p-two": "#005792",
+        "p-three": "#fcfcfc",
+        "p-four": "#fd5f00",
+        // changing "white-w-..." to "w-..."
+        "w-one": "#f6fceb",
+        "w-two": "#e6f5ff",
+        "steelblue": {
+          "100": "#267db8",
+          "200": "rgba(38, 125, 184, 0.09)",
         },
-        orangered: '#e34500',
-        'white-w-one': '#f6fceb',
+        "orangered": "#e34500",
       },
-      spacing: {},
       fontFamily: {
-        'para-date': "'Roboto Serif'",
+        "para-date": "'Roboto Serif'",
       },
     },
     fontSize: {
-      xs: '12px',
-      '29xl': '48px',
-      '19xl': '38px',
-      '10xl': '29px',
-      '14xl': '33px',
-      '7xl': '26px',
-      xl: '20px',
-      '21xl': '40px',
-      '13xl': '32px',
-      '5xl': '24px',
-      '9xl': '28px',
-      '3xl': '22px',
-      lg: '18px',
-      '4xl': '23px',
-      inherit: 'inherit',
+      // re-arrange sizes
+      "xs": "12px",
+      "lg": "18px",
+      "xl": "20px",
+      "3xl": "22px",
+      "4xl": "23px",
+      "5xl": "24px",
+      "7xl": "26px",
+      "9xl": "28px",
+      "10xl": "29px",
+      "13xl": "32px",
+      "14xl": "33px",
+      "19xl": "38px",
+      "21xl": "40px",
+      "29xl": "48px",
+      "inherit": "inherit",
     },
     screens: {
-      mq1450: {
-        raw: 'screen and (max-width: 1450px)',
+      // adding screens and re-arrage
+      "2sm": {
+        max: "375px",
       },
-      lg: {
-        max: '1200px',
+      "sm": {
+        max: "425px",
       },
-      mq825: {
-        raw: 'screen and (max-width: 825px)',
+      "md": {
+        max: "768px",
       },
-      mq450: {
-        raw: 'screen and (max-width: 450px)',
+      "lg": {
+        max: "1024px",
+      },
+      "xl": {
+        max: "1280px",
+      },
+      "mq1450": {
+        raw: "screen and (max-width: 1450px)",
+      },
+      "mq825": {
+        raw: "screen and (max-width: 825px)",
+      },
+      "mq450": {
+        raw: "screen and (max-width: 450px)",
       },
     },
   },
