25255e0780eedbfeac5696f391a9d0b2d6bb1fec
Gustitreshana <gustith2105@gmail.com>
2024-08-14 03:07:48 +0700

    feat: add dropdown menu when profile clicked

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,27 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const Navbar = () => {
 
+  const [dropdownVisible, setDropdownVisible] = useState(false);
   const router = useRouter();
   const handleJelajahiKomunitas = () => {
     router.push("/nyaripedagang");
+  };
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleProfile = () => {
+    router.push("/profile-user");
+  };
+
+  const handleLogout = () => {
+    console.log("Nanti hilangin token biar logout");
   };
 
   return (
@@ -44,14 +58,32 @@ const Navbar = () => {
           <p className="text-[12px]">Pedagang Jagung</p>
         </div>
 
-        <div>
+        <div className="relative">
           <Image
             className="rounded-full w-[40px] h-[40px] bg-image bg-cover bg-center object-cover cursor-pointer"
             src="/foto-munaroh.svg"
             alt="user"
             width={40}
             height={40}
+            onClick={toggleDropdown}
           />
+          {dropdownVisible && (
+            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={handleProfile}>
+                  <p>Profile</p>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                  <p>Setting</p>
+                </li>
+                <li className="px-4 py-2 text-red-500 hover:bg-gray-100 cursor-pointer"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
