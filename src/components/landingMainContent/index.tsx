091fc4a7f141b6b3cc74d953acd2833c052085a2
isnendyankp <isnendyankp@gmail.com>
2024-08-15 15:34:57 +0700

    Add: button link for jump to each section

--- src/components/landingMainContent/index.tsx
+++ src/components/landingMainContent/index.tsx
@@ -81,64 +81,81 @@ const LandingMainContain = () => {
       {/* Benefit Section */}
       <section
         id="benefit-section"
-        className="w-[1520px] rounded-[50px] bg-white-w-two overflow-hidden flex flex-col items-center justify-start py-[100px] px-5 box-border gap-[100px] max-w-full text-center text-21xl text-black font-para-date gap-[50px] mq825:pt-[65px] mq825:pb-[65px] mq825:box-border gap-[25px]"
+        className="w-[1520px] rounded-[50px] bg-w-two overflow-hidden flex flex-col items-center justify-start py-[100px] px-5 box-border gap-[100px] max-w-full text-center text-21xl text-black font-para-date gap-[50px] mq825:pt-[65px] mq825:pb-[65px] mq825:box-border gap-[25px]"
       >
         <h3 className="m-0 w-[1096px] relative text-inherit leading-[46px] font-semibold font-[inherit] inline-block max-w-full mq825:text-13xl mq825:leading-[37px] mq450:text-5xl mq450:leading-[28px]">
           Fitur Spesial Untukmu!
         </h3>
 
         <div className="w-[1185px] overflow-x-auto flex flex-row items-center justify-between gap-[-200px] max-w-full text-lg">
-          <div
-            className={`w-[200px] shrink-0 flex flex-col items-end justify-start gap-[13px] z-[4] text-center text-9xl text-black font-para-date`}
-          >
-            <Image
-              className="relative overflow-hidden shrink-0 object-cover"
-              src="/cardbenefit-1@2x.png"
-              alt="Temukan Kebutuhan"
-              width={200}
-              height={195}
-            />
-            <div className="self-stretch relative leading-[34px] mq450:text-3xl mq450:leading-[27px]">
-              Temukan Kebutuhan
+          {/* Temukan Kebutuhan Card Benefit */}
+          <a href="#Temukan-Kebutuhan" className="block">
+            <div
+              className={`w-[200px] shrink-0 flex flex-col items-end justify-start gap-[13px] z-[4] text-center text-9xl text-black font-para-date`}
+            >
+              <Image
+                className="relative overflow-hidden shrink-0 object-cover"
+                src="/cardbenefit-1@2x.png"
+                alt="Temukan Kebutuhan"
+                width={200}
+                height={195}
+              />
+              <div className="self-stretch relative leading-[34px] mq450:text-3xl mq450:leading-[27px]">
+                Temukan Kebutuhan
+              </div>
             </div>
-          </div>
-          <div
-            className={`w-[200px] shrink-0 flex flex-col items-end justify-start gap-[13px] z-[4] text-center text-9xl text-black font-para-date`}
-          >
-            <Image
-              className="relative overflow-hidden shrink-0 object-cover"
-              src="/cardbenefit-2@2x.png"
-              alt="Temukan Pembeli"
-              width={200}
-              height={195}
-            />
-            <div className="self-stretch relative leading-[34px] mq450:text-3xl mq450:leading-[27px]">
-              Temukan Pembeli
+          </a>
+
+          {/* Temukan Pembeli Card Benefit */}
+          <a href="#Temukan-Pembeli" className="block">
+            <div
+              className={`w-[200px] shrink-0 flex flex-col items-end justify-start gap-[13px] z-[4] text-center text-9xl text-black font-para-date`}
+            >
+              <Image
+                className="relative overflow-hidden shrink-0 object-cover"
+                src="/cardbenefit-2@2x.png"
+                alt="Temukan Pembeli"
+                width={200}
+                height={195}
+              />
+              <div className="self-stretch relative leading-[34px] mq450:text-3xl mq450:leading-[27px]">
+                Temukan Pembeli
+              </div>
             </div>
-          </div>
-          <div
-            className={`w-[200px] shrink-0 flex flex-col items-end justify-start gap-[13px] z-[4] text-center text-9xl text-black font-para-date`}
-          >
-            <Image
-              className="relative overflow-hidden shrink-0 object-cover"
-              src="/cardbenefit-3@2x.png"
-              alt="Info UMKM"
-              width={200}
-              height={195}
-            />
-            <div className="self-stretch relative leading-[34px] mq450:text-3xl mq450:leading-[27px]">
-              Info UMKM
+          </a>
+
+          {/* Info UMKM Card Benefit */}
+          <a href="#Info-UMKM" className="block">
+            <div
+              className={`w-[200px] shrink-0 flex flex-col items-end justify-start gap-[13px] z-[4] text-center text-9xl text-black font-para-date`}
+            >
+              <Image
+                className="relative overflow-hidden shrink-0 object-cover"
+                src="/cardbenefit-3@2x.png"
+                alt="Info UMKM"
+                width={200}
+                height={195}
+              />
+              <div className="self-stretch relative leading-[34px] mq450:text-3xl mq450:leading-[27px]">
+                Info UMKM
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       </section>
 
       {/* Benefit List with description Section */}
-      <section className="w-[1520px] rounded-[50px] overflow-hidden flex flex-col items-center justify-start py-[100px] px-5 box-border gap-[100px] max-w-full text-center text-21xl text-black font-para-date gap-[50px] mq825:pt-[65px] mq825:pb-[65px] mq825:box-border gap-[25px]">
-        <div className="w-[1520px] flex flex-row items-start justify-start gap-[50px] max-w-full gap-[25px] mq1450:flex-wrap">
+      <section className="w-[1520px] rounded-[50px] overflow-hidden flex flex-col items-center justify-start py-[100px] px-5 box-border gap-[100px] max-w-full text-center  font-para-date gap-[50px] mq825:pt-[65px] mq825:pb-[65px] mq825:box-border gap-[25px]">
+        {/*  Temukan Kebutuhan */}
+        <div
+          id="Temukan-Kebutuhan"
+          className="w-[1520px] flex flex-row items-start justify-start gap-[50px] max-w-full gap-[25px] mq1450:flex-wrap"
+        >
           <div className="flex-1 flex flex-col items-center justify-start pt-[118.5px] px-0 pb-0 box-border min-w-[695px] max-w-full mq825:min-w-full">
-            <h4>Temukan Kebutuhan</h4>
-            <div>
+            <h4 className="text-29xl text-black font-semibold">
+              Temukan Kebutuhan
+            </h4>
+            <div className="text-14xl text-black">
               Fitur yang memungkinkan UMKM untuk mencari dan menemukan produk
               atau layanan yang mereka butuhkan
             </div>
@@ -152,7 +169,11 @@ const LandingMainContain = () => {
           />
         </div>
 
-        <div className="w-[1520px] flex flex-row items-start justify-start gap-[50px] max-w-full gap-[25px] mq1450:flex-wrap">
+        {/* Temukan Pembeli */}
+        <div
+          id="Temukan-Pembeli"
+          className="w-[1520px] flex flex-row items-start justify-start gap-[50px] max-w-full gap-[25px] mq1450:flex-wrap"
+        >
           <Image
             className="relative max-h-full shrink-0 object-cover max-w-full mq1450:flex-1"
             src="/frame-80-2@2x.png"
@@ -161,18 +182,24 @@ const LandingMainContain = () => {
             height={400}
           />
           <div className="flex-1 flex flex-col items-center justify-start pt-[118.5px] px-0 pb-0 box-border min-w-[695px] max-w-full mq825:min-w-full">
-            <h4>Temukan Pembeli</h4>
-            <div>
+            <h4 className="text-29xl text-black font-semibold">
+              Temukan Pembeli
+            </h4>
+            <div className="text-14xl text-black">
               Fitur yang memungkinkan UMKM untuk menemukan peluang bisnis dengan
               melihat kebutuhan pelaku UMKM lainnya
             </div>
           </div>
         </div>
 
-        <div className="w-[1520px] flex flex-row items-start justify-start gap-[50px] max-w-full gap-[25px] mq1450:flex-wrap">
+        {/* Info UMKM */}
+        <div
+          id="Info-UMKM"
+          className="w-[1520px] flex flex-row items-start justify-start gap-[50px] max-w-full gap-[25px] mq1450:flex-wrap"
+        >
           <div className="flex-1 flex flex-col items-center justify-start pt-[118.5px] px-0 pb-0 box-border min-w-[695px] max-w-full mq825:min-w-full">
-            <h4>Info UMKM</h4>
-            <div>
+            <h4 className="text-29xl text-black font-semibold">Info UMKM</h4>
+            <div className="text-14xl text-black">
               Fitur yang memungkinkan UMKM untuk mendapatkan informasi terkait
               UMKM lainnya
             </div>
