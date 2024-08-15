25f8234e6ed7619da51cf39ad2d02e42f2fb7822
isnendyankp <isnendyankp@gmail.com>
2024-08-15 20:15:59 +0700

    Fix: styling with add layout for landingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import React from 'react';
-import LandingNavbar from '@/components/landingNavbar';
-import LandingMainContent from '@/components/landingMainContent';
-import { Toaster } from 'react-hot-toast';
-import Footer from '@/components/Footer';
+import { redirect } from 'next/navigation'
+
 
 const landingpage = () => {
-  return (
-    <div
-      className={`w-[1920px] bg-primary-p-three max-w-full overflow-hidden flex flex-col items-start justify-start leading-[normal] tracking-[normal]`}
-    >
-      <LandingNavbar />
-      <LandingMainContent />
-      <Toaster/>
-      <Footer />
-    </div>
-  );
+  return redirect('/landing');
 };
 
 export default landingpage;
