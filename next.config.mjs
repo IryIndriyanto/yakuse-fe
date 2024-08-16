7c350b50d4575d64d09d268ddc1593c4527d96b5
Gustitreshana <gustith2105@gmail.com>
2024-08-16 22:35:27 +0700

    feat: add config for external images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'revou-finalproject-backend-production.up.railway.app',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
