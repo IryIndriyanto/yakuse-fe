5d9f4521195be8eabb46702a75071fb6f62767a5
arisarae <playarisa24@gmail.com>
2024-08-17 13:59:21 +0700

    fix minor merge error

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,7 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: 'example.com', 
-        },
-        {
-          protocol: 'https',
-          hostname: 'example.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'example.com',
-        },
+        }
       ],
     },
   };
