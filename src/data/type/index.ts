4704064bd1ca0ff9e044ef8e1ab904c536ec3835
arisarae <playarisa24@gmail.com>
2024-08-28 20:21:10 +0700

    fix mock data and type

--- src/data/type/index.ts
+++ src/data/type/index.ts
@@ -1,14 +1,14 @@
 export interface profilType {
-  id: string;
-  username: string;
-  email: string;
-  fullname: string;
-  phone: string;
-  address: string;
-  photo_url: string;
-  about_me: string;
-  created_at: string;
-  updated_at: string;
+  id: string,
+  username: string,
+  email: string,
+  fullname: string,
+  phone: string,
+  address: string,
+  photo_url: string,
+  about_me_list: string[],
+  created_at: string,
+  updated_at: string
 }
 
 export interface bisnisType {
@@ -34,43 +34,43 @@ export interface bisnisType {
       id: string,
       rating_count: number,
       review_description: string,
-      business_name: string,
       rater_name: string
     }
   ]
 }
 
 export interface categoryType {
-  id: string;
-  name: string;
-  describe: string;
-  created_at: string;
+  id: string,
+  name: string,
+  describe: string,
+  created_at: string
 }
 
 export interface ratingType {
-  id: string;
-  rating_count: number;
-  review_description: string;
-  created_at: string;
-  updated_at: string;
-  business_name: string;
-  rater_name: string;
+  id: string,
+  rating_count: number,
+  review_description: string,
+  created_at: string,
+  updated_at: string,
+  business_name: string,
+  rater_name: string
 }
 
 export interface permintaanType {
-  id: string;
-  title: string;
+  id: number,
+  title: string,
   user_info: {
-    user_id: number;
-    owner_username: string;
-    user_profile_url: string;
-  };
-  description: string;
-  is_visible: boolean;
+    user_id: string,
+    fullname: string,
+    username: string,
+    user_profile_url: string
+  },
+  description: string,
+  is_visible: true,
   category: {
-    id: string;
-    name: string;
-  };
-  created_at: string;
-  updated_at: string;
+    id: number,
+    name: string,
+  },
+  created_at: string,
+  updated_at: string
 }
