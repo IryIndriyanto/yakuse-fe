35ee161a42bccb6b710fb61b5f68f9dc04548f1f
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-09-03 05:41:05 -0700

    fix: add bussiness category in user need form

--- src/app/(form)/need-form/page.tsx
+++ src/app/(form)/need-form/page.tsx
@@ -17,26 +17,28 @@ const EditProfile = () => {
   const router = useRouter();
 
   const categories = [
-    "Industri",
-    "Kreatif",
-    "Pertanian",
-    "Teknologi",
-    "Pendidikan",
-    "Transportasi",
-    "Properti",
-    "Kuliner",
+    "kuliner",
+    "industri",
+    "kreatif",
+    "jasa",
+    "pertanian",
+    "teknologi",
+    "pendidikan",
+    "kesehatan",
+    "transportasi",
+    "properti",
   ];
 
   const initialValues = {
     title: "",
     description: "",
-    businessTag: "",
+    fk_business_category_id: 1,
   };
 
   const validationSchema = Yup.object({
     title: Yup.string().required("Title wajib diisi."),
     description: Yup.string().required("Deskripsi wajib diisi."),
-    businessTag: Yup.string(),
+    fk_business_category_id: Yup.number(),
   });
 
   async function handleSubmit(values: typeof initialValues) {
@@ -74,7 +76,7 @@ const EditProfile = () => {
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
         >
-          {({ values, handleChange, handleSubmit }) => {
+          {({ values, handleChange, handleSubmit, setFieldValue }) => {
             return (
               <form
                 onSubmit={handleSubmit}
@@ -114,7 +116,24 @@ const EditProfile = () => {
                     className="text-red-500 text-[14px] font-[500] h-[20px] w-full"
                   />
                 </div>
-
+                <div className="flex flex-wrap justify-center gap-4">
+                  {categories.map((category, index) => (
+                    <button
+                      type="button"
+                      key={index}
+                      className={`border border-gray-400 rounded-full px-4 py-2 text-gray-700 transition ${
+                        values.fk_business_category_id === index + 1
+                          ? "bg-blue-400 text-white"
+                          : "hover:bg-gray-200"
+                      }`}
+                      onClick={() =>
+                        setFieldValue("fk_business_category_id", index + 1)
+                      }
+                    >
+                      {category}
+                    </button>
+                  ))}
+                </div>
                 <FormButton
                   text="Submit"
                   type="submit"
