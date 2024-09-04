6c9274b10c6b139d4790b6bcd4a9db9ad583f1cc
Gustitreshana <gustith2105@gmail.com>
2024-09-05 01:29:26 +0700

    feat: add interface OtherUserBusiness

--- src/components/ProfileCardOtherUser/types.tsx
+++ src/components/ProfileCardOtherUser/types.tsx
@@ -10,3 +10,29 @@ export interface OtherUserProfile {
   created_at: string;
   updated_at: string;
 }
+
+export interface OtherUserBusiness {
+  id: string;
+  name: string;
+  omset: number;
+  description_list: string[];
+  photo_url: string;
+  location: string;
+  contact: string;
+  created_at: string;
+  updated_at: string;
+  category: string;
+  owner_info: {
+    user_id: string;
+    fullname: string;
+    photo_url: string;
+  };
+  avg_rating: number;
+  total_rater: number;
+  rating_list: {
+    id: string;
+    rating_count: number;
+    review_description: string;
+    rater_name: string;
+  }[];
+}
