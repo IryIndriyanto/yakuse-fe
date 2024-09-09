dcfbf20ce4dc1f461a9fd41a8450d7a9a2ef65fd
Gustitreshana <gustith2105@gmail.com>
2024-09-09 16:28:32 +0700

    style: add z index 50

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -106,7 +106,7 @@ const Navbar = () => {
             onClick={toggleDropdown}
           />
           {dropdownVisible && (
-            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-50">
               <ul>
                 <li
                   className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
