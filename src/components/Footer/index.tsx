fd1d049167f3765709f97529b859817d028608a8
unknown <playarisa24@gmail.com>
2024-09-14 01:47:23 +0700

    fix minor

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-<footer className="w-full bg-[#FCFCFC] py-8 px-8 sm:px-2 lg:px-[24px] flex justify-between border-t-[1px] border-[#FD5F00] inset-x-0 bottom-0">
+    <footer className="w-full bg-[#FCFCFC] py-8 px-8 sm:px-2 lg:px-[24px] flex justify-between border-t-[1px] border-[#FD5F00] inset-x-0 bottom-0">
       <div className="flex flex-col justify-center gap-4">
         <div>
-        <h1 className="text-[38px] text-blue-400 font-bold md:text-[32px]">YAKUSE</h1>
+          <h1 className="text-[38px] text-blue-400 font-bold md:text-[32px]">YAKUSE</h1>
         </div>
       </div>
 
